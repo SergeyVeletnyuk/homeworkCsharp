359363aa2cd9aa2ee9ac8c6ff267534fb1a71263
Sergey <sergeyveletnyuk57@gmail.com>
2022-11-21 21:50:00 +0300

    task3 finished

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num %2 == 0)
+{
+    Console.WriteLine("Это число четное");
+
+}
+else
+{
+    Console.WriteLine("Это число нечетное");
+}
