f3fc2b58880419de1c04e436b1d79895b3d06049
Sergey <sergeyveletnyuk57@gmail.com>
2022-11-21 22:18:22 +0300

    finished task4

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Console.WriteLine("Введите число");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// int i = 1;
+
+// while (i <= N)
+// {
+//     if(i % 2 == 0)
+//     {
+//         Console.WriteLine(i);
+//     }
+//     i+=1;
+// }
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i = 1; i <= N; i++ )
+{
+    if (i%2==0)
+    {
+        Console.WriteLine(i);
+    }
+}
