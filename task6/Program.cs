a013bb9d83c78c599abed8e5cab7839dadb7b130
Sergey <sergeyveletnyuk57@gmail.com>
2022-11-26 16:27:48 +0300

    task6 finished

---
+++ task6/Program.cs
@@ -0,0 +1,33 @@
+﻿// задача 13
+
+int a = new Random().Next(1, 100000);
+Console.WriteLine(a);
+if (a>99 && a<1000)
+{
+    int b = a % 10;
+    Console.WriteLine(b);
+}
+else if (a>1000 && a < 10000)
+{
+    int count = a%100;
+    int b1 = count/10;
+    Console.WriteLine(b1);
+}
+else if (a>10000 && a<100000)
+{
+    int count2 = a%1000;
+    int b2 = count2/100;
+    Console.WriteLine(b2);
+}
+
+// int a = new Random().Next(1, 10000);
+// Console.WriteLine(a);
+// string a1 = Convert.ToString(a);
+// if (a1.Length > 2)
+// {
+//     Console.WriteLine("третья цифра: " + a1[2]);
+// }
+// else
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
