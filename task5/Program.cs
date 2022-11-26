b4ff9c673293112364314c3543d75d22d8279764
Sergey <sergeyveletnyuk57@gmail.com>
2022-11-26 16:25:21 +0300

    task5 finished

---
+++ task5/Program.cs
@@ -0,0 +1,8 @@
+﻿// задача 10
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+int count = num % 100 ;
+int count2 = count/10;
+Console.WriteLine("Вторая цифра этого числа: ");
+Console.WriteLine(count2);
