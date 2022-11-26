83dc9ff6d671916996befa750a5ede7c8cc1dc16
Sergey <sergeyveletnyuk57@gmail.com>
2022-11-26 16:28:53 +0300

    task7 finished

---
+++ task7/Program.cs
@@ -0,0 +1,20 @@
+﻿//задача 15
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a);
+if (a == 6)
+{
+
+    Console.WriteLine("Это суббота, выходной день");
+}
+else if (a == 7)
+{
+    Console.WriteLine("Это воскресенье, выходной день");
+}
+else if (a < 6 )
+{
+    Console.WriteLine("Рабочий день");
+}
+else
+{
+    Console.WriteLine("Это уже не дни недели, их всего 7");
+}
