db92d7eeac0d67fb9f3ce06b76a25d9e2f6ffafe
Sergey <sergeyveletnyuk57@gmail.com>
2022-11-27 12:10:27 +0300

    new while

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,24 +1,24 @@
 ﻿// задача 13
 
-int a = new Random().Next(1, 100000);
-Console.WriteLine(a);
-if (a>99 && a<1000)
-{
-    int b = a % 10;
-    Console.WriteLine(b);
-}
-else if (a>1000 && a < 10000)
-{
-    int count = a%100;
-    int b1 = count/10;
-    Console.WriteLine(b1);
-}
-else if (a>10000 && a<100000)
-{
-    int count2 = a%1000;
-    int b2 = count2/100;
-    Console.WriteLine(b2);
-}
+// int a = new Random().Next(1, 100000);
+// Console.WriteLine(a);
+// if (a>99 && a<1000)
+// {
+//     int b = a % 10;
+//     Console.WriteLine(b);
+// }
+// else if (a>1000 && a < 10000)
+// {
+//     int count = a%100;
+//     int b1 = count/10;
+//     Console.WriteLine(b1);
+// }
+// else if (a>10000 && a<100000)
+// {
+//     int count2 = a%1000;
+//     int b2 = count2/100;
+//     Console.WriteLine(b2);
+// }
 
 // int a = new Random().Next(1, 10000);
 // Console.WriteLine(a);
@@ -31,3 +31,22 @@ else if (a>10000 && a<100000)
 // {
 //     Console.WriteLine("Третьей цифры нет");
 // }
+
+int a = new Random().Next(1, 10000);
+Console.WriteLine(a);
+int index =0;
+while(a>999)
+{
+    a/=10;
+    index++;
+}
+if (a<999 && a > 99)
+{
+    int b = a % 10;
+    
+    Console.WriteLine($"Третья цифра числа: {b}, ");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
