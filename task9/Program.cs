eb4763fa19d2d29c69bd962e86ab1128499ef574
Сергей Велетнюк <sergejveletnuk@MacBook-Pro-Sergej.local>
2022-11-30 20:57:14 +0300

    new task9 finished

--- task9/Program.cs
+++ task9/Program.cs
@@ -11,18 +11,24 @@
 // string N = Convert.ToString (num);
 // if (N[0] == N[4] && N[1] == N[3])
 // {
-// Console.WriteLine ("Это число является палиндромом") ;
+// // Console.WriteLine ("Это число является палиндромом") ;
 
 // }
 // else
 // {
-// Console.WriteLine ("Это число не является палиндромом") ;
+// // Console.WriteLine ("Это число не является палиндромом") ;
 // }
 
 
 Console.WriteLine("Введите пятизначное числож: ");
-int num = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
+if(num1/10000 == num1%10 && (num1/1000)%10 == (num1/10)%10 )
+{
+    Console.WriteLine("Это число является палиндромом");
 
-string N = Convert.ToString(num);
-
+}
+else
+{
+    Console.WriteLine("Это число не является палиндромом");
+}
 
