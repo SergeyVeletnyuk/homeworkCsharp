3ebd70c41c7e247269638a5b8cb956dad387a115
Сергей Велетнюк <sergejveletnuk@MacBook-Pro-Sergej.local>
2022-11-30 20:58:13 +0300

    new task10 finished

--- task10/Program.cs
+++ task10/Program.cs
@@ -4,21 +4,25 @@
 // 5 -> 1, 8, 27, 64, 125
 // 3 -> 1, 8, 27
 // 5-> 1. 8. 27.64.125
-//Console.Write("Введите число N: ");
+
+
+// Console.Write("Введите число N: ");
 // int N = Convert.ToInt32(Console.ReadLine ());
 // int i = 1;
 // while(i < N)
-//{
+// {
 // int result = (int)Math. Pow(i, 3);
 // i++;
-// Console.WriteLine(result);
-//}
+// // Console.WriteLine(result);
+// }
+
+
 
 Console.Write ("Введите число N: ");
-int N = Convert.ToInt32 (Console.ReadLine());
-int i = 1;
-while(i < N)
+int N1 = Convert.ToInt32 (Console.ReadLine());
+int j = 1;
+while(j < N1)
 {
-Console.WriteLine($"{i*i*i}");
-i++;
+Console.WriteLine($"{j*j*j}");
+j++;
 }
