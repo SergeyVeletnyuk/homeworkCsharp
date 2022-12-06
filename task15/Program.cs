b9c60da194002c35a086e5af556a4da428f3400b
Сергей Велетнюк <sergejveletnuk@MacBook-Pro-Sergej.local>
2022-12-06 20:07:50 +0300

    task15 finished

---
+++ task15/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] Array(int len)
+{
+    int[] array = new int[len];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 20);
+    }
+    return array;
+}
+
+int[] arr = Array(10);
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+PrintArray(arr);
+int sum = 0;
+for(int i = 0; i < arr.Length; i++)
+{
+    if(i%2 != 0)
+    {
+        sum = sum + arr[i];
+    
+    }
+    
+    
+}
+Console.WriteLine();
+Console.WriteLine($"сумма цифр на нечетных позициях равна {sum}");
