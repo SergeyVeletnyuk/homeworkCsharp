e67a57e12d11d27dc8afdeac24c87abeda91907d
Сергей Велетнюк <sergejveletnuk@MacBook-Pro-Sergej.local>
2022-12-13 20:43:51 +0300

    task19 finished

---
+++ task19/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте двумерный массив размером mхn,
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 - 0,2
+// 1-3,3 8 - 9,9
+// 8 7,8 -7,1 9
+
+double [,] matrix = new double[3, 4];
+
+void PrintMatrix(double[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"    {matrix[i, j]}    ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void NewMatrix(double[,] matrix)
+{
+    
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 10)/10.0;
+
+            
+        }
+    }
+}
+
+PrintMatrix(matrix);
+NewMatrix(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
