b740db366cd69bac6c8f0354aa5d537178e1a361
Сергей Велетнюк <sergejveletnuk@MacBook-Pro-Sergej.local>
2022-12-20 22:14:39 +0300

    task27 finished

---
+++ task27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значения М и N. Напишите программу, которая выведет все натуральные числа в промежутке от М до N.
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 6, 7,8"
+
+Console.WriteLine("введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Numbers(int m, int n)
+{
+    if(m==n) Console.Write($" {m} ");
+    else
+    {
+        Numbers(m, n-1);
+        Console.Write($" {n} ");
+    }
+}
+if (m > n)
+{
+    Numbers(n,m);
+
+}
+else
+{
+    Numbers(m,n);
+}
